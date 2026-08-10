82b996401935455f0ad020dd85f9893acd67014b
root <root@JacksPC>
2026-08-10 14:52:21 +0200

    X

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,150 +1,183 @@
 #include <iostream>
 
+bool minusCheck(const std::string& input, size_t &i);
+bool plusCheck(const std::string &input, size_t &i);
+
 int main(int ac, char **av)
 {
 	if(ac != 2)
 	{
-		std::cerr << "Too many arguments!\n";
+		std::cerr << "Only 2 arguments allowed!\n";
 		return 1;
 	}
 
 	std::string input = av[1];
 	size_t len = input.length();
-	bool found = false;
-	int minusFlag = 0;
-	if(input[0] == '.')
+	int minus = 0;
+	int f = 0;
+	int dec = 0;
+	int plus = 0;
+	bool foundDigit = false;
+	
+	// Validate chars
+	for(size_t i = 0; i < len; i++)
 	{
-		std::cerr << "Invalid input '.'\n";
+		if(!isalpha(input[i]) && !isdigit(input[i]) && input[i] != '.' 
+		&& input[i] != 'f' && input[i] != '-' && input[i] != '+')
+		{
+			std::cerr << "Invalid char found in input0\n";
+			return 1;	
+		}
+	}
+	if(input[0] == '+' && input[1] == '-')
+	{
+		std::cerr << "Invalid input\n";
 		return 1;
 	}
 
+	// Validate char positions
 	for(size_t i = 0; i < len; i++)
 	{
-		if(!isalpha(input[i]) && !isdigit(input[i]) && input[i] != '.' && input[i] != 'f' && input[0] != '-')
+		if(!isalpha(input[i]) && !isdigit(input[i]) && input[i] != '.' 
+		&& input.back() != 'f' && input[0] != '-' && input[0] != '+')
 		{
-			std::cerr << "Invalid char found in input\n";
+			std::cerr << "Invalid char found in input1\n";
+			return 1;
+		}
+		if(input[i] == 'f')
+			f++;
+		if(input[i] == '-')
+			minus++;
+		if(input[i] == '.')
+			dec++;
+		if(input[i] == '+')
+			plus++;
+		if(f > 1 || minus > 1 || dec > 1 || plus > 1)
+		{
+			std::cerr << "Invalid input00\n";
 			return 1;
 		}
 	}
 
-	for(size_t j = 0; j < len; j++)
+	// Invalid single chars
+	if(len == 1 && !isalpha(input[0]) && !isdigit(input[0]))
 	{
-		//Char check
-		if(len == 1 && isalpha(input[0]))
-		{
-			std::cout << "Char\n";
-			found = true;
-			break;
-		}
-		if(found == true)
-			break;
-
-		//Int check
-		for(size_t i = 0; i < len; i++)
-		{
-			if(input[i] == '-')
-			{
-				minusFlag++;
-				if(minusFlag > 1)
-				{
-					std::cerr << "Too many '-'\n";
-					return 1;
-				}
-				i++;
-			}
-			if(!isdigit(input[i]))
-				break;
-			if(i == (len - 1))
-			{
-				std::cout << "Int\n";
-				found = true;
-				break;
-			}	
-		}
-		if(found == true)
-			break;
-		minusFlag = 0;
-		//Float check
-		int fHasDec = 0;
-		bool fDecFlag = false;
-		for(size_t i = 0; i < len; i++)
-		{
-			if(input[i] == '-')
-			{
-				minusFlag++;
-				if(minusFlag > 1)
-				{
-					std::cerr << "Too many '-'\n";
-					return 1;
-				}
-			}
-			if(isalpha(input[i]) && i != (len - 1))
-			{
-				std::cerr << "Invalid input1!\n";
-				return 1;
-			}
-			if(input[i] == '.')
-			{	
-				fHasDec++;
-				fDecFlag = true;
-				if(fHasDec > 1)
-				{
-					std::cerr << "Invalid input2!\n";
-						return 1;
-				}
-			}
-			if(i == (len - 1) && input.back() == 'f' && (fDecFlag == true))
-			{
-				std::cout << "Float\n";
-				found = true;
-				break;
-			}
-			if(i == (len - 1) && input.back() == 'f' && (fDecFlag == false))
-			{
-				std::cerr << "Invalid input3\n";
-				return 1;
-			}
-		}
-		if(found == true)
-			break;
-		minusFlag = 0;
-		//Double check
-		int dHasDec = 0;
-		bool dDecFlag = false;
-		for(size_t i = 0; i < len; i++)
-		{
-			if(input[i] == '-')
-			{
-				minusFlag++;
-				if(minusFlag > 1)
-				{
-					std::cerr << "Invalid input 4\n";
-					return 1;
-				}
-			}
-			if(isalpha(input[i]))
-			{
-				std::cerr << "Invalid input5!\n";
-				return 1;
-			}
-			if(input[i] == '.')
-			{	
-				dHasDec++;
-				dDecFlag = true;
-				if(dHasDec > 1)
-				{
-					std::cerr << "Invalid input6!\n";
-						return 1;
-				}
-			}
-			if(i == (len - 1) && (dDecFlag == true) && input[len - 1] != '.')
-			{
-				found = true;
-				std::cout << "Double\n";
-			}
-		}
-		if(found == true)
-			break;
+		std::cout << "Invalid char '" << input[0] << "' found in input2\n";
+		return 1;
 	}
+
+	// Char check
+	if(len == 1 && isalpha(input[0]))
+	{
+		std::cout << "Char\n";
+		return 0;
+	}
+
+	// Int Validation
+	for(size_t i = 0; i < len; i++)
+	{
+		if(isdigit(input[i]))
+			foundDigit = true;
+	}
+	if(foundDigit == false)
+	{
+		std::cerr << "No digit found\n";
+		return 1;
+	}
+
+	// Int check
+	for(size_t i = 0; i < len; i++)
+	{
+		if(!minusCheck(input, i))
+			return 1;
+		if(!plusCheck(input, i))
+			return 1;
+		if(!isdigit(input[i]))
+			break;
+		if(i == (len - 1))
+		{
+			std::cout << "Int\n";
+			return 0;
+		}	
+	}
+
+	// Float check
+	bool decFlag = false;
+	for(size_t i = 0; i < len; i++)
+	{
+		if(!minusCheck(input, i))
+			return 1;
+		if(!plusCheck(input, i))
+			return 1;
+		if(isalpha(input[i]) && i != (len - 1))
+		{
+			std::cerr << "Invalid input1!\n";
+			return 1;
+		}
+		if(input[i] == '.')
+			decFlag = true;
+		if(i == (len - 1) && input.back() == 'f' && (decFlag == true))
+		{
+			std::cout << "Float\n";
+			return 0;
+		}
+		if(i == (len - 1) && input.back() == 'f' && (decFlag == false))
+		{
+			std::cerr << "Invalid input3\n";
+			return 1;
+		}
+	}
+
+	// Double check
+	decFlag = false;
+	for(size_t i = 0; i < len; i++)
+	{
+		// std::cout << "Checking Double\n";
+		if(!minusCheck(input, i))
+			return 1;
+		if(!plusCheck(input, i))
+			return 1;
+		if(isalpha(input[i]))
+		{
+			std::cerr << "Invalid input5!\n";
+			return 1;
+		}
+		if(input[i] == '.')
+			decFlag = true;
+		if(i == (len - 1) && (decFlag == true))
+		{
+			std::cout << "Double\n";
+			return 0;
+		}
+	}
+
 	return 0;
+}
+
+bool minusCheck(const std::string &input, size_t &i)
+{
+	if(input[i] == '-')
+	{
+		if(i != 0)
+		{
+			std::cerr << "Invalid char detected-\n";
+			return false;
+		}
+		i++;
+	}
+	return true;
+}
+
+bool plusCheck(const std::string &input, size_t &i)
+{
+	if(input[i] == '+')
+	{
+		if(i != 0)
+		{
+			std::cerr << "Invalid char detected+\n";
+			return false;
+		}
+		i++;
+	}
+	return true;
 }
