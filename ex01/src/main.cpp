0ecb44ab76b45b12b716bf4fa636928392e2196b
root <root@JacksPC>
2026-08-18 15:25:11 +0200

    x

---
+++ ex01/src/main.cpp
@@ -0,0 +1,29 @@
+#include "../include/Serializer.hpp"
+
+int main(void)
+{
+	Data data;
+	Data* ptr = &data;
+
+	data.name = "Test";
+	data.number = 5;
+
+	std::cout << "Pointer: " << ptr << "\n";
+	uintptr_t ptrInt = Serializer::serialize(ptr);
+	std::cout << "Serialization: " << ptrInt << "\n";
+
+	Data *raw = Serializer::deserialize(ptrInt);
+	std::cout << "Deserialization: " << raw << "\n";
+
+	if(ptr == raw)
+		std::cout << "Pointers are equal\n";
+	else
+		std::cout << "Pointers are not equal\n";
+
+	std::cout << "Ptr name: " << ptr->name << "\n";
+	std::cout << "Ptr number: " << ptr->number << "\n";
+
+	std::cout << "Raw name: " << raw->name << "\n";
+	std::cout << "Raw number: " << raw->number << "\n";
+	return 0;
+}
